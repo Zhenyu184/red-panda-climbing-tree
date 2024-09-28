2ed7e0d08027d8de08ad22b010622a9012de3159
Zhenyu184 <zyy1120126@gmail.com>
2024-09-28 20:06:09 +0800

    feat: add rete.js package

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,14 @@ import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
+import { createEditor } from "./editor";
+import { useRete } from "rete-react-plugin";
+
 function App() {
     const [outputMsg, setOutputMsg] = useState("");
     const [name, setName] = useState("");
+
+    const [ref] = useRete(createEditor);
 
     async function handle1() {
         setOutputMsg(await invoke("handler1", { name }));
@@ -23,7 +28,14 @@ function App() {
         <div className="container">
             <h1>Welcome to Tauri!</h1>
 
-            <div className="row">
+            <div className="App">
+                <div
+                    ref={ref}
+                    style={{ height: "100vh", width: "100vw" }}
+                ></div>
+            </div>
+
+            <div className="editor-space">
                 <a href="https://vitejs.dev" target="_blank">
                     <img
                         src="/vite.svg"
