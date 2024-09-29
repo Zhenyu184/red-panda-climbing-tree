9cf8cbb70193503e43ad35df0924a8e965de9275
Zhenyu184 <zyy1120126@gmail.com>
2024-09-29 13:49:16 +0800

    feat: add clock component

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,115 @@
-// react and tauri
-import "./App.css";
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
+import './App.css';
+import { useState, useEffect } from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
 
 // rete.js
-import { createEditor } from "./editor";
-import { useRete } from "rete-react-plugin";
+import { createEditor } from './editor';
+import { useRete } from 'rete-react-plugin';
 
 // assets
-import reactLogo from "./assets/react.svg";
+import reactLogo from './assets/react.svg';
+
+function Clock() {
+    const [dateTime, setDateTime] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setDateTime(new Date());
+        }, 1000);
+
+        return () => {
+            clearInterval(timer);
+        };
+    }, []);
+
+    const formatDate = (date: any) => {
+        const options = {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+            weekday: 'short',
+        };
+        return date.toLocaleDateString('zh-TW', options);
+    };
+
+    const formatTime = (date: any) => {
+        const options = {
+            hour: '2-digit',
+            minute: '2-digit',
+            second: '2-digit',
+            hour12: false,
+        };
+        return date.toLocaleTimeString('zh-TW', options);
+    };
+
+    return (
+        <div className='clock'>
+            <div className='date'>{formatDate(dateTime)}</div>
+            <div className='time'>{formatTime(dateTime)}</div>
+        </div>
+    );
+}
 
 function App() {
-    const [name, setName] = useState("");
-    const [outputMsg, setOutputMsg] = useState("");
+    const [name, setName] = useState('');
+    const [outputMsg, setOutputMsg] = useState('');
     const [editor] = useRete(createEditor);
 
     async function handle1() {
-        setOutputMsg(await invoke("handler1", { name }));
+        setOutputMsg(await invoke('handler1', { name }));
     }
 
     async function handle2() {
-        setOutputMsg(await invoke("handler2", { name }));
+        setOutputMsg(await invoke('handler2', { name }));
     }
 
     async function handle3() {
-        setOutputMsg(await invoke("handler3", { name }));
+        setOutputMsg(await invoke('handler3', { name }));
     }
 
     return (
-        <div className="container">
-            <div className="sidebar">
+        <div className='container'>
+            <div className='sidebar'>
                 <h1>Hi Rad panda!</h1>
-                <div className="row">
-                    <a href="https://vitejs.dev" target="_blank">
-                        <img src="/vite.svg" className="logo vite" alt="Vite logo" />
+
+                <div className='row'>
+                    <a href='https://vitejs.dev' target='_blank'>
+                        <img src='/vite.svg' className='logo vite' alt='Vite logo' />
                     </a>
-                    <a href="https://tauri.app" target="_blank">
-                        <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
+                    <a href='https://tauri.app' target='_blank'>
+                        <img src='/tauri.svg' className='logo tauri' alt='Tauri logo' />
                     </a>
-                    <a href="https://reactjs.org" target="_blank">
-                        <img src={reactLogo} className="logo react" alt="React logo" />
+                    <a href='https://reactjs.org' target='_blank'>
+                        <img src={reactLogo} className='logo react' alt='React logo' />
                     </a>
                 </div>
 
                 <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
-                <form className="row">
-                    <input
-                        id="greet-input"
-                        onChange={(e) => setName(e.currentTarget.value)}
-                        placeholder="Enter a name..."
-                    />
+                <form className='row'>
+                    <input id='greet-input' onChange={(e) => setName(e.currentTarget.value)} placeholder='Enter a name...' />
                 </form>
 
-                <div className="row">
-                    <button type="button" onClick={() => handle1()} className="greet-button">
+                <div className='row'>
+                    <button type='button' onClick={() => handle1()} className='greet-button'>
                         Greet
                     </button>
-                    <button type="button" onClick={() => handle2()} className="greet-button">
+                    <button type='button' onClick={() => handle2()} className='greet-button'>
                         Start
                     </button>
-                    <button type="button" onClick={() => handle3()} className="greet-button">
+                    <button type='button' onClick={() => handle3()} className='greet-button'>
                         test
                     </button>
                 </div>
 
                 <p>{outputMsg}</p>
+                <div className='sidebar-footer'>
+                    <Clock />
+                </div>
             </div>
 
-            <div className="editor-space">
-                <div ref={editor} className="rete" style={{ height: "100%", width: "100%" }}></div>
+            <div className='editor-space'>
+                <div ref={editor} className='rete' style={{ height: '100%', width: '100%' }}></div>
             </div>
         </div>
     );
--- src/editor.tsx
+++ src/editor.tsx
@@ -1,65 +1,63 @@
-import { createRoot } from "react-dom/client";
-import { NodeEditor, GetSchemes, ClassicPreset } from "rete";
-import { AreaPlugin, AreaExtensions } from "rete-area-plugin";
-import { ConnectionPlugin, Presets as ConnectionPresets } from "rete-connection-plugin";
-import { ReactPlugin, Presets, ReactArea2D } from "rete-react-plugin";
-import { AutoArrangePlugin, Presets as ArrangePresets } from "rete-auto-arrange-plugin";
-import { DataflowEngine } from "rete-engine";
-import { ContextMenuExtra, ContextMenuPlugin, Presets as ContextMenuPresets } from "rete-context-menu-plugin";
+import { createRoot } from 'react-dom/client';
+import { NodeEditor, GetSchemes, ClassicPreset } from 'rete';
+import { AreaPlugin, AreaExtensions } from 'rete-area-plugin';
+import { ConnectionPlugin, Presets as ConnectionPresets } from 'rete-connection-plugin';
+import { ReactPlugin, Presets, ReactArea2D } from 'rete-react-plugin';
+import { AutoArrangePlugin, Presets as ArrangePresets } from 'rete-auto-arrange-plugin';
+import { DataflowEngine } from 'rete-engine';
+import { ContextMenuExtra, ContextMenuPlugin, Presets as ContextMenuPresets } from 'rete-context-menu-plugin';
 
-const socket = new ClassicPreset.Socket("socket");
+const socket = new ClassicPreset.Socket('socket');
 
 class NumberNode extends ClassicPreset.Node<
     {},
     { value: ClassicPreset.Socket },
-    { value: ClassicPreset.InputControl<"number"> }
+    { value: ClassicPreset.InputControl<'number'> }
 > {
-    height = 120;
-    width = 180;
+    height = 135;
+    width = 200;
 
     constructor(initial: number, change?: () => void) {
-        super("Number");
-        this.addControl("value", new ClassicPreset.InputControl("number", { initial, change }));
-        this.addOutput("value", new ClassicPreset.Output(socket, "Number"));
+        super('Number');
+        this.addControl('value', new ClassicPreset.InputControl('number', { initial, change }));
+        this.addOutput('value', new ClassicPreset.Output(socket, 'Number'));
     }
 
     data(): { value: number } {
-        return {
-            value: this.controls.value.value || 0,
-        };
+        return { value: this.controls.value.value || 0 };
     }
 }
 
 class AddNode extends ClassicPreset.Node<
     { left: ClassicPreset.Socket; right: ClassicPreset.Socket },
     { value: ClassicPreset.Socket },
-    { value: ClassicPreset.InputControl<"number"> }
+    { value: ClassicPreset.InputControl<'number'> }
 > {
-    height = 190;
-    width = 180;
+    height = 220;
+    width = 200;
 
-    constructor(change?: () => void, private update?: (control: ClassicPreset.InputControl<"number">) => void) {
-        super("Add");
-        const left = new ClassicPreset.Input(socket, "Left");
-        const right = new ClassicPreset.Input(socket, "Right");
+    constructor(change?: () => void, private update?: (control: ClassicPreset.InputControl<'number'>) => void) {
+        super('Add');
+        const left = new ClassicPreset.Input(socket, 'Left');
+        const right = new ClassicPreset.Input(socket, 'Right');
 
-        left.addControl(new ClassicPreset.InputControl("number", { initial: 0, change }));
-        right.addControl(new ClassicPreset.InputControl("number", { initial: 0, change }));
+        left.addControl(new ClassicPreset.InputControl('number', { initial: 0, change }));
+        right.addControl(new ClassicPreset.InputControl('number', { initial: 0, change }));
 
-        this.addInput("left", left);
-        this.addInput("right", right);
+        this.addInput('left', left);
+        this.addInput('right', right);
         this.addControl(
-            "value",
-            new ClassicPreset.InputControl("number", {
+            'value',
+            new ClassicPreset.InputControl('number', {
                 readonly: true,
             })
         );
-        this.addOutput("value", new ClassicPreset.Output(socket, "Number"));
+        this.addOutput('value', new ClassicPreset.Output(socket, 'Number'));
     }
 
     data(inputs: { left?: number[]; right?: number[] }): { value: number } {
-        const leftControl = this.inputs.left?.control as ClassicPreset.InputControl<"number">;
-        const rightControl = this.inputs.right?.control as ClassicPreset.InputControl<"number">;
+        const leftControl = this.inputs.left?.control as ClassicPreset.InputControl<'number'>;
+        const rightControl = this.inputs.right?.control as ClassicPreset.InputControl<'number'>;
 
         const { left, right } = inputs;
         const value = (left ? left[0] : leftControl.value || 0) + (right ? right[0] : rightControl.value || 0);
@@ -99,8 +97,8 @@ export async function createEditor(container: HTMLElement) {
 
     const contextMenu = new ContextMenuPlugin<Schemes>({
         items: ContextMenuPresets.classic.setup([
-            ["Number", () => new NumberNode(0, process)],
-            ["Add", () => new AddNode(process, (c) => area.update("control", c.id))],
+            ['Number', () => new NumberNode(0, process)],
+            ['Add', () => new AddNode(process, (c) => area.update('control', c.id))],
         ]),
     });
     area.use(contextMenu);
@@ -126,7 +124,7 @@ export async function createEditor(container: HTMLElement) {
     AreaExtensions.showInputControl(area);
 
     editor.addPipe((context) => {
-        if (["connectioncreated", "connectionremoved"].includes(context.type)) {
+        if (['connectioncreated', 'connectionremoved'].includes(context.type)) {
             process();
         }
         return context;
@@ -134,10 +132,10 @@ export async function createEditor(container: HTMLElement) {
 
     const a = new NumberNode(1, process);
     const b = new NumberNode(1, process);
-    const c = new AddNode(process, (c) => area.update("control", c.id));
+    const c = new AddNode(process, (c) => area.update('control', c.id));
 
-    const con1 = new Connection(a, "value", c, "left");
-    const con2 = new Connection(b, "value", c, "right");
+    const con1 = new Connection(a, 'value', c, 'left');
+    const con2 = new Connection(b, 'value', c, 'right');
 
     await editor.addNode(a);
     await editor.addNode(b);
@@ -149,7 +147,5 @@ export async function createEditor(container: HTMLElement) {
     await arrange.layout();
     AreaExtensions.zoomAt(area, editor.getNodes());
 
-    return {
-        destroy: () => area.destroy(),
-    };
+    return { destroy: () => area.destroy() };
 }
